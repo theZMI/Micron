5927408ae6619ec5ef139d5c352e2f6ad9eaa78c
MicronEngine <zmigpost@gmail.com>
2014-01-25 05:26:22 +0300

    Update _500.php

--- lang/en/_500.php
+++ lang/en/_500.php
@@ -2,5 +2,5 @@
 
     $g_lang['m_title'] = '500 Internal Server Error';
     $g_lang['head']    = 'Site Error';
-    $g_lang['message'] = 'Sorry, but we have an error on the site.';
+    $g_lang['message'] = 'Sorry, but there is an error on the site.';
 ?>
