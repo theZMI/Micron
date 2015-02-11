2edf985ef68b3b932668b87623a720c6bd1e8713
Volkodav-vvs <vvsynchak@i.ua>
2015-02-11 16:58:06 +0200

    Update msg.php
    Убрал добавление jquery Мешает при динамической прогрузке элементов страницы ибо все элементы уже загружены а дополнительная загрузка вызывает хрен знает что с DOMом

--- tpl/dev/msg.php
+++ tpl/dev/msg.php
@@ -1,5 +1,4 @@
 
-    <?php IncludeCom('dev/jquery')?>
     <head>
         <link rel="stylesheet" type="text/css" href="<?= Root('i/css/dev/msg.css')?>" />
         <script type="text/javascript" src="<?= Root('i/js/dev/msg.js')?>"></script>
